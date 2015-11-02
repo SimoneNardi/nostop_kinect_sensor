992a688b8bef3636b8b349200837eff877ec33fd
NikoGiovannini <nikogiovannini3@gmail.com>
2015-11-02 18:57:57 +0100

    Main node

--- src/sensor_node.cpp
+++ src/sensor_node.cpp
@@ -2,26 +2,30 @@
 #include "Collector.h"
 #include "Collection.h"
 #include "Tracker.h"
+#include "Robot_manager.h"
 
 int main(int argc, char **argv)
 {
 	ROS_INFO("Sensor node : ON");
 	
 	
+	
 	ros::init(argc, argv, "sensor"); 
+	
 	
 	// Compute Robot Position:
 	// From Kinect?ROS_INFO
 	// From Simulator?
 	
 	Robotics::GameTheory::Collector l_Collector;
-	
-	l_Collector.start();
+	//Robotics::GameTheory::Robot_manager Manager;
 	ros::NodeHandle n;
-
-	ros::spin();
+	while(ros::ok())
+	{
+	  ros::spinOnce();
+	  l_Collector.pack_passage();
+	}
 	
-	l_Collector.stop();
      
 	return 0;
 }
