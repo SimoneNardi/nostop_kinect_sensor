ec261e0e229f8b558bd36f8a0e183ec011114961
NikoGiovannini <nikogiovannini3@gmail.com>
2018-11-10 14:54:59 +0100

    Implement open loop kalman tracker

--- include/nostop_kinect_sensor/Tracker.h
+++ include/nostop_kinect_sensor/Tracker.h
@@ -28,20 +28,13 @@ namespace Robotics
 			cv::KalmanFilter m_kf;
 			cv::Mat m_state;
 			cv::Mat m_meas;
-			cv::Mat m_drawCircle;
 			double m_ticks;
-			int m_notFoundCount = 0;
-// 			rosbag::Bag m_bag;
 			ros::NodeHandle tracker;
-			ros::Publisher m_pub_position;
-		protected:
-			
+
 		public:
 			Tracker();
 			void matrixSettings(cv::KalmanFilter m_kf);
-			cv::Rect kalman_update(ball_position position,bool found);
-// 			void write2bag(std::string color);
-// 			void bag2read(std::string color);
+			cv::Rect kalman_update(ball_position& position);
 
 			
 			~Tracker();
--- src/Tracker.cpp
+++ src/Tracker.cpp
@@ -30,7 +30,6 @@ Tracker::Tracker() :
 m_kf(m_stateSize,m_measSize,m_contrSize,type)
 , m_state(m_stateSize,1,type)
 , m_meas(m_measSize,1,type)
-// , m_found(true)
 {
   matrixSettings(m_kf);
 }
@@ -56,10 +55,26 @@ void Tracker::matrixSettings(cv::KalmanFilter m_kf)
  
    // Measures Noise Covariance Matrix R
    cv::setIdentity(m_kf.measurementNoiseCov, cv::Scalar(1e-1));
+   
+   // >>>> Initialization
+      m_kf.errorCovPre.at<float>(0) = 1; // px
+      m_kf.errorCovPre.at<float>(7) = 1; // px
+      m_kf.errorCovPre.at<float>(14) = 1;m_kf.statePost = m_state;
+      m_kf.errorCovPre.at<float>(21) = 1;
+      m_kf.errorCovPre.at<float>(28) = 1; // px
+      m_kf.errorCovPre.at<float>(35) = 1; // px
+
+      m_state.at<float>(0) = m_meas.at<float>(0);
+      m_state.at<float>(1) = m_meas.at<float>(1);
+      m_state.at<float>(2) = 0;
+      m_state.at<float>(3) = 0;
+      m_state.at<float>(4) = m_meas.at<float>(2);
+      m_state.at<float>(5) = m_meas.at<float>(3);
+      // <<<< Initialization
 }
 
 
-cv::Rect Tracker::kalman_update(ball_position position,bool found) 
+cv::Rect Tracker::kalman_update(ball_position& position) 
 {
   double precTick = m_ticks;
   m_ticks = (double) cv::getTickCount();
@@ -67,133 +82,21 @@ cv::Rect Tracker::kalman_update(ball_position position,bool found)
   cv::Rect predRect;
   
       
+   m_kf.transitionMatrix.at<float>(2) = dT;
+   m_kf.transitionMatrix.at<float>(9) = dT;
+   cv::Point center;          
+   center.x = m_state.at<float>(0);          
+   center.y = m_state.at<float>(1);
       
-      if (!found)// TO DO 
-      {
-         m_notFoundCount++;
-	 if( m_notFoundCount >= 10 )
-	  {
-            found = false;
-	  }else{
-		  m_kf.statePost = m_state;
-		}
-      }else{
-	    m_notFoundCount = 0;
- 
-	    m_meas.at<float>(0) = position.x + position.width / 2;
-	    m_meas.at<float>(1) = position.y + position.height / 2;
-	    m_meas.at<float>(2) = (float)position.width;
-	    m_meas.at<float>(3) = (float)position.height;
-
-	    if (!found) // First detection!
-	    {
-	      // >>>> Initialization
-	      m_kf.errorCovPre.at<float>(0) = 1; // px
-	      m_kf.errorCovPre.at<float>(7) = 1; // px
-	      m_kf.errorCovPre.at<float>(14) = 1;m_kf.statePost = m_state;
-	      m_kf.errorCovPre.at<float>(21) = 1;
-	      m_kf.errorCovPre.at<float>(28) = 1; // px
-	      m_kf.errorCovPre.at<float>(35) = 1; // px
- 
-	      m_state.at<float>(0) = m_meas.at<float>(0);
-	      m_state.at<float>(1) = m_meas.at<float>(1);
-	      m_state.at<float>(2) = 0;
-	      m_state.at<float>(3) = 0;
-	      m_state.at<float>(4) = m_meas.at<float>(2);
-	      m_state.at<float>(5) = m_meas.at<float>(3);
-	      // <<<< Initialization
- 
-	      found = true;   
-         }else{
-	       m_kf.correct(m_meas); // Kalman Correction
-	      }
-    if (found) //TO DO 
-      {
-         m_kf.transitionMatrix.at<float>(2) = dT;
-         m_kf.transitionMatrix.at<float>(9) = dT;
-         m_state = m_kf.predict();     
-	 predRect.width = m_state.at<float>(4);          
-	 predRect.height =  m_state.at<float>(5);          
-	 predRect.x = m_state.at<float>(0) - predRect.width / 2;          
-	 predRect.y = m_state.at<float>(1) - predRect.height / 2;            
-	 cv::Point center;          
-	 center.x = m_state.at<float>(0);          
-	 center.y = m_state.at<float>(1);
-	 return predRect;
-
-      }
-      
-      }
+    m_meas.at<float>(0) = position.x + position.width / 2;
+    m_meas.at<float>(1) = position.y + position.height / 2;
+    m_meas.at<float>(2) = (float)position.width;
+    m_meas.at<float>(3) = (float)position.height;
+    m_kf.correct(m_meas); // Kalman Correction
+    m_state = m_kf.predict();     
+    predRect.width = m_state.at<float>(4);          
+    predRect.height =  m_state.at<float>(5);          
+    predRect.x = m_state.at<float>(0) - predRect.width / 2;          
+    predRect.y = m_state.at<float>(1) - predRect.height / 2;          
+    return predRect;
 }
-
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- /*
-void Tracker::write2bag(std::string color)
-{
-    if(m_close){
-      m_bag.open(color+".bag",rosbag::bagmode::Write);
-      m_close = false;
-    }
-    if (m_found)
-    {
-      std_msgs::Float32 x_bag,y_bag;
-      float x,y;
-      x = m_state.at<float>(0);
-      y = m_state.at<float>(1);
-      x_bag.data = x;
-      y_bag.data = y;
-      m_bag.write<std_msgs::Float32>("x",ros::Time::now(),x_bag);
-      m_bag.write<std_msgs::Float32>("y",ros::Time::now(),y_bag);
-      } else{
-	if(m_notFoundCount>=10){
-	    m_bag.close();
-	    m_close = true;
-	  }else{
-	     std_msgs::Float32 x_bag,y_bag;
-	    float x,y;
-	    x = m_state.at<float>(0);
-	    y = m_state.at<float>(1);
-	    x_bag.data = x;
-	    y_bag.data = y;
-	    m_bag.write<std_msgs::Float32>("x",ros::Time::now(),x_bag);
-	    m_bag.write<std_msgs::Float32>("y",ros::Time::now(),y_bag);
-	    }
-      } 
-}
-      
-void Tracker::bag2read(std::string color)
-{ 
-  m_bag.open(color+".bag",rosbag::bagmode::Read);
-  float x;
-  std::vector<std::string> topics;
-  topics.push_back(std::string("x"));
-  topics.push_back(std::string("y"));
-  
-  rosbag::View view(m_bag, rosbag::TopicQuery(topics));
-  foreach(rosbag::MessageInstance const m,view)
-  {
-    std_msgs::Float32::ConstPtr s = m.instantiate<std_msgs::Float32>();
-    x = s->data;
-    
-    ROS_INFO("%f",x);
-  }
-  m_bag.close();
-}
-*/
